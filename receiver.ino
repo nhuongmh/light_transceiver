62aa403f8ddcc528b7dbf163f7a104f019ff66ea
Minh Nhuong <proton207@gmail.com>
2017-06-20 19:27:49 +0700

    initial commit

---
+++ receiver.ino
@@ -0,0 +1,47 @@
+const char heso =6;
+String rx_str;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(7,INPUT_PULLUP);
+Serial.begin(9600);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  byte i,data=0x00;
+while(1)
+{ if(digitalRead(7)==0)
+    {  delay(7*heso);
+      if(digitalRead(7)==1)
+      { //while(digitalRead(7)==1);
+        delay(3*heso);
+        for(i=0;i<8;i++)
+     {delay(7*heso);
+      if(digitalRead(7)==1)
+   { data+=1;
+    if(i!=7)
+    data<<=1; 
+    //while(digitalRead(7)==1);
+    delay(8*heso); }
+    else 
+    {if(i!=7)
+    data<<=1;
+    /*if(i!=7) {
+    while(digitalRead(7)==0); 
+    while(digitalRead(7)==1);}*/
+    delay(8*heso); }
+  }
+        }
+      //  else 
+       // Serial.print("failed");
+      if((data>=0x20)&&(data<=0x7F))
+       Serial.write(data);
+     // Serial.print("\n");
+      break;
+      }
+
+  }
+
+
+}
---
+++ transmitter.ino
@@ -0,0 +1,66 @@
+byte kytu;
+const char factor = 6 ;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(7,INPUT_PULLUP);
+  pinMode(8, OUTPUT);
+}
+void transmit(byte kytu)
+{ int i;
+  digitalWrite(8,HIGH);
+delay(5*factor);
+digitalWrite(8,LOW);
+delay(5*factor);
+
+  Serial.write(kytu);
+for(i=0;i<8;i++)
+{ if(kytu&0x80)
+    { digitalWrite(8,HIGH);
+    delay(5*factor);
+    digitalWrite(8,LOW);
+    delay(10*factor);  }
+    else 
+    {digitalWrite(8,HIGH);
+    delay(10*factor);
+    digitalWrite(8,LOW);
+      delay(5*factor);
+    }
+    kytu<<=1;
+  }
+    delay(200);
+  }
+void loop() {
+  // put your main code here, to run repeatedly:
+byte i,jk='N';
+
+
+//delay(5000);
+
+  //Serial.print("\n");
+ if(digitalRead(7)==0) {
+  delay(5);
+  if(digitalRead(7)==0) {  
+    while(Serial.available()!=0) {
+      kytu=Serial.read(); 
+    transmit(kytu);
+}
+delay(2000);
+while(1)
+{ transmit(' ');  
+transmit('-');  
+transmit('-');  
+transmit('-');  
+  transmit('h');
+    transmit('c');
+    transmit('m');
+    transmit('u');
+    transmit('t');  
+    
+  }
+ }
+}
+}
+ 
+
+
